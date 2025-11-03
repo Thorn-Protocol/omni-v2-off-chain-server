f4ca1d2ea94a8432d2fef7b3afbce24e1533cd03
phuongnguyenlaaithe <phuongnguyen2522003@gmail.com>
2025-11-03 10:44:14 +0700

    change address offchain strategy

--- src/common/config/config.ts
+++ src/common/config/config.ts
@@ -11,7 +11,7 @@ export const addresses = {
   },
   usdcV2OnBase: {
     vault: "0x2669DfA1D91c1dF9fe51DEAC6E5369C7D43242a8",
-    offChainStrategy: "0xD2a9dB8f22707166e82EdF89534340237780eDA3",
+    offChainStrategy: "0x00aa576bfa5f75BC6C651e8Cb587dD78b287040A",
     strategy: {
       wasabi: "0x1C4a802FD6B591BB71dAA01D8335e43719048B24",
     },
