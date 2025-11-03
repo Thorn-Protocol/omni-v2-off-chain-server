f68b3e93a708bfd5c8aeefc10e3def3bfae700f4
phuongnguyenlaaithe <phuongnguyen2522003@gmail.com>
2025-11-03 18:17:40 +0700

    refactor code

--- src/lib/winston.ts
+++ src/lib/winston.ts
@@ -2,13 +2,6 @@ import winston from "winston";
 import TelegramLogger from "winston-telegram";
 import { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } from "../common/config/secrets";
 
-if (!TELEGRAM_BOT_TOKEN) {
-  throw new Error("Missing TELEGRAM_BOT_TOKEN in environment variables");
-}
-if (!TELEGRAM_CHAT_ID) {
-  throw new Error("Missing TELEGRAM_CHAT_ID in environment variables");
-}
-
 const logger = winston.createLogger({
   level: "debug",
   format: winston.format.json(),
